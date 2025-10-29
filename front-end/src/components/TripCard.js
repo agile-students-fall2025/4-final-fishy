679718e6da2df5dbf0f437d263ccec00c1588655
Amyliao0 <yl11020@nyu.edu>
2025-10-28 21:44:24 -0400

    feat(trips): Trip dashboard + overview drawer wiring (#5)

--- front-end/src/components/TripCard.js
+++ front-end/src/components/TripCard.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { formatPrettyDate } from "../utils/helpers";
 
-function TripCard() {
+export default function TripCard({ trip, onOpen }) {
+  const { destination, startDate, endDate, days } = trip;
+  const totalActs = days.reduce((s, d) => s + d.activities.length, 0);
+
   return (
-    <div>
-      <h2>Trip Card Component</h2>
-    </div>
+    <button className="tm-card" onClick={() => onOpen?.(trip)} aria-label={`Open ${destination}`}>
+      <div className="tm-card__header">
+        <h3 className="tm-card__title">{destination}</h3>
+        <div className="tm-card__date">
+          {formatPrettyDate(startDate)} – {formatPrettyDate(endDate)}
+        </div>
+      </div>
+      <div className="tm-card__body">
+        <div className="tm-card__stat">
+          <span className="tm-card__stat-num">{days.length}</span>
+          <span className="tm-card__stat-label">Days</span>
+        </div>
+        <div className="tm-card__stat">
+          <span className="tm-card__stat-num">{totalActs}</span>
+          <span className="tm-card__stat-label">Activities</span>
+        </div>
+      </div>
+    </button>
   );
 }
-
-export default TripCard;
