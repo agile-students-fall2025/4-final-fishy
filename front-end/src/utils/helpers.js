ed56c3637b7640ccec4ac4d582cfe849d58d779d
Amyliao0 <yl11020@nyu.edu>
2025-10-28 22:38:45 -0400

    Implementing task #25 from user story #3: Design Trip creation form (frontend)

--- front-end/src/utils/helpers.js
+++ front-end/src/utils/helpers.js
@@ -1,1 +1,11 @@
 // Utility functions will go here
+export function formatPrettyDate(iso) {
+    if (!iso) return "";
+    const d = new Date(iso);
+    return d.toLocaleDateString(undefined, { year: "numeric", month: "short", day: "numeric" });
+  }
+  
+  export function makeId(prefix = "id") {
+    return `${prefix}_${Math.random().toString(36).slice(2, 8)}`;
+  }
+  
